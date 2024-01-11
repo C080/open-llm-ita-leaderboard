6653795506529a6535adc7756383cfdcff7242d4
financialsupport <marvin994@gmail.com>
2024-01-11 22:57:02 +0100

    updated saving on hf

--- main.py
+++ main.py
@@ -2,97 +2,101 @@ import os
 from model import load_model, Agent
 from read_esco import read_dataset
 from utils import print_colored
-from datasets import Dataset, load_from_disk, concatenate_datasets
+from datasets import Dataset, load_dataset, concatenate_datasets
 import pandas as pd
 
 def main():
 
     # Config
+    overwrite_mode = True
     max_job_per_occupation = 3
     similarity_threshold = 0.8
     data_path = os.path.dirname(os.path.abspath(__file__)) # TODO da standardizzare
     model_path = "E:/text-generation-webui-main/models/saiga-7b"
-    output_path = "E:/uni/thesis/output"
 
-    # Check if the dataset file exists
-    if os.path.exists(output_path):
-        # Load existing dataset
-        existing_dataset = load_from_disk(output_path)
-    else:
-        # Initialize an empty dataset
-        existing_dataset = Dataset.from_pandas(pd.DataFrame(columns=['memory']))
+    # load dataset from huggingface
+    existing_dataset = load_dataset('FinancialSupport/SynthEscoJobAds')
 
     occupations = read_dataset(data_path)
     model, tokenizer = load_model(model_path)
 
     # Initialize agent
-    agent_1 = Agent(model, tokenizer, 'cuda') #create job description
-    agent_2 = Agent(model, tokenizer, 'cuda') #quality check of the job description
+    Writer = Agent(model, tokenizer, 'cuda') #create job description
+    Supervisor = Agent(model, tokenizer, 'cuda') #quality check of the job description
 
     # Iterate over occupations
     for i, row in occupations.iterrows():
 
+        #Check if 'preferredLabel' is already in the dataset
+        if (existing_dataset.filter(lambda example: example['escoLabel'] == row['preferredLabel']).num_rows['train'] > 0) or (overwrite_mode == False): 
+            print_colored(f"[Boss]: {row['preferredLabel']} already in the dataset. Proceed with the next topic", "red")
+            continue
+
         # Reset agents memory
-        agent_1.forget()
-        agent_2.forget()
+        Writer.forget()
+        Supervisor.forget()
         j = 0
 
-        print_colored(f"Boss: incoming task #{i+1} for Agent 1", "red")
+        print_colored(f"[Boss]: incoming task #{i+1} for the team", "red")
 
         # Construct task string
         generate_task = f"Generate a realistic job description for the role of {row['preferredLabel']},\
                 \nwe are talking about a {row['description']}. It is also known as:\n{row['altLabels']}."
 
-        print_colored(f"Agent 1: receiving task #{i+1}:", "blue")
+        print_colored(f"[Writer]: receiving task #{i+1}:", "blue")
         print_colored(generate_task, "blue")
 
         # Generate job descriptions
-        answers = agent_1.answer(generate_task, batch_generate = max_job_per_occupation)
-        print_colored(f"\nAgent 1: task #{i+1} executed {max_job_per_occupation} times", "green")
+        answers = Writer.answer(generate_task, batch_generate = max_job_per_occupation)
+        print_colored(f"\n[Writer]: jd creation task #{i+1} executed {max_job_per_occupation} times", "green")
         #print_colored('\n\n@@END@@\n\n'.join(answers), "green")
 
         # Save job descriptions in memory of agents 2
-        agent_1.memorize(answers)
-        agent_2.memorize(agent_1.remember())
+        Writer.memorize(answers)
+        Supervisor.memorize(Writer.remember())
 
         # Iterate over job descriptions
         while True:
 
             # Check if job descriptions are too similar
-            jd_too_similar = agent_2.find_similarity_in_memory(similarity_threshold)
+            jd_too_similar = Supervisor.find_similarity_in_memory(similarity_threshold)
             if not jd_too_similar:
                 break 
             jd_pair_too_similar = jd_too_similar[0] 
 
-            print_colored(f"Boss: incoming task #{j+1} for Agent 2", "red")
             # Construct task string
             supervise_task = f"Repharse this job description: {answers[jd_pair_too_similar[1]]},\
             \nbecause it is too similar to this other job description: {answers[jd_pair_too_similar[2]]}.\
             \nRememebre that we are talking about a {row['description']}. It is also known as:\n{row['altLabels']}."
-            print_colored(f"Agent 2: supervising task #{i+1}. Found two job description {jd_pair_too_similar[0]*100}% similar.", "blue")
+            print_colored(f"[Supervisor]: Found two similar job description in task #{i+1} ({jd_pair_too_similar[0]*100}%).", "blue")
             #print_colored(supervise_task, "blue")
 
-            fix = agent_2.answer(supervise_task, batch_generate=1)
-            print_colored(f"\nAgent 2: task #{j+1} fixed", "green")
+            fix = Supervisor.answer(supervise_task, batch_generate=1)
+            print_colored(f"\n[Supervisor]: task #{j+1} fixed", "green")
             #print_colored(fix, "green")
 
             # Refresh memory of agents 2
-            old_memory = agent_2.remember()
+            old_memory = Supervisor.remember()
             new_memory = old_memory
             new_memory[jd_pair_too_similar[1]] = fix
-            agent_2.memorize(new_memory)
+            Supervisor.memorize(new_memory)
 
             # Update counter
             j += 1
         
         # Save memory of agent 2 in a huggingface dataset
-        final_memory = agent_2.remember()
+        final_memory = Supervisor.remember()
+        # Add 'preferredLabel' to each job ad
+        final_memory_dicts = [{'job_ad': job_ad, 'escoLabel': row['preferredLabel']} for job_ad in final_memory]
         # Convert the list of memories into a Hugging Face Dataset
-        new_data = Dataset.from_pandas(pd.DataFrame(final_memory, columns=['memory']))
+        new_data = Dataset.from_pandas(pd.DataFrame(final_memory_dicts))
         # Concatenate the existing dataset with the new data
-        updated_dataset = concatenate_datasets([existing_dataset, new_data])
+        updated_dataset = concatenate_datasets([existing_dataset['train'], new_data])
         # Save the dataset
-        updated_dataset.save_to_disk(output_path)
+        if overwrite_mode:
+            new_data.push_to_hub('FinancialSupport/SynthEscoJobAds')
+        else:
+            updated_dataset.push_to_hub('FinancialSupport/SynthEscoJobAds')
 
         break
 
